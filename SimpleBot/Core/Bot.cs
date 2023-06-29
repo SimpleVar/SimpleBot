f9936c711a20fbdad8e945a231d066b96fdcbf0a
SimpleVar <13912845+SimpleVar@users.noreply.github.com>
2023-06-29 22:53:20 +0300

    jajajajaja

--- SimpleBot/Core/Bot.cs
+++ SimpleBot/Core/Bot.cs
@@ -925,6 +925,8 @@ namespace SimpleBot
             try
             {
               using var http = new HttpClient();
+              // chesscum wants useragent
+              http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246");
               var chesscum = JToken.Parse(http.GetStringAsync("https://api.chess.com/pub/player/" + targetName + "/stats").Result);
               if (chesscum != null)
               {
